7b8e7b1e0be82c69875dae8325fd6add7e100616
Jorge Lopez <jlobeto@gmail.com>
2019-07-07 11:30:11 -0500

    Strings

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -32,4 +32,21 @@ fun main(args: Array<String>) {
     //a++
     println("a ${a.inc()}")
 
+    //Strings
+    val nombre = "Jorge"
+    val apellido: String = "López"
+
+    println("Tu nombre es: " + nombre + " " + apellido)
+    println("Tu nombre es: $nombre $apellido")  //propio de Kotlin
+
+    //raw String
+    val parrafo: String = """
+        Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer
+        took a galley of type and scrambled it to make a type specimen book.
+    """.trimIndent()
+
+    println(parrafo)
+
 }
